c9d05609a2ac808149508dd2c2e9805add77c1ac
Arkes <mateusaqles@gmail.com>
2023-10-20 14:12:35 -0300

    Initial commit of Pomodorable

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Pomodorable
+{
+    
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // Adicione um manipulador de eventos para o evento Activated
+            this.Activated += MainWindow_Activated;
+        }
+
+        // Manipulador de eventos para o evento Activated
+        private void MainWindow_Activated(object sender, EventArgs e)
+        {
+            // Coloque o código que você deseja executar quando a janela entra em foco aqui.
+            // Isso será acionado quando a janela se tornar a janela ativa.
+        }
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# dotnet-Pomodorable
+ A simple native pomodoro desktop app built with .NET and WPF. An app made just to test my skills
